e06936b51444a267e0b77ed944d7788a4c4b4c45
status-im-auto <auto@status.im>
2026-08-10 06:30:06 +0000

    Update documentation

---
+++ postscript-cad920a4.js
@@ -0,0 +1,12 @@
+await Promise.all([
+  import("./static/scripts/script-0-ec880d15.js"),
+  import("./static/scripts/script-1-1f9394e6.js"),
+  import("./static/scripts/script-2-2a102aaa.js"),
+  import("./static/scripts/script-3-cc8afa9d.js"),
+  import("./static/scripts/script-4-7e107d7e.js"),
+  import("./static/scripts/script-5-0adb5274.js"),
+  import("./static/scripts/script-6-f7c52b6a.js"),
+  import("./static/scripts/script-7-5f71ef20.js"),
+  import("./static/scripts/script-8-a2f22c29.js")
+]);
+await import("./static/scripts/script-9-7ca62114.js");
